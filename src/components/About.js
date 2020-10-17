8352d0a59073567e0cfd6dbfa9e5130bca2f1636
Stella Wang <stellaw767@gmail.com>
2020-10-17 19:39:48 +0000

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+  
+import React from 'react';
+import '../App.css';
+import './about.css'
+
+
+ function About() {
+  return (
+    <div className = "on">
+    
+    <div className='about__container'>
+    <div className='about__wrapper'>
+    
+          
+    
+    </div>
+    </div>
+    </div>
+
+
+    
+  );
+}
+
+
+export default About 
+
+
---
+++ src/components/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+import About from '../About';
+
+
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <About/>
+      
+      <Footer />
+      
+      
+    </>
+  );
+}
+
+export default Home;
